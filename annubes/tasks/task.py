486e49de1dbff6937b7476a7dbcfd2839d8e944b
gcroci2 <crocioni.giulia@gmail.com>
2024-01-23 17:15:39 +0100

    add todos and functionalities

--- annubes/tasks/task.py
+++ annubes/tasks/task.py
@@ -1,78 +1,72 @@
 import itertools
 import random
+import colorsys
 import sys
 import numpy as np
+from collections import OrderedDict
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
+# TODO: Complete docstrings
+# TODO: Evaluate https://docs.pydantic.dev/latest/ for input validation
 class Task:
     """General class for defining a task."""
     def __init__(self,
                  name: str,
-                 modalities: list[str] = ['v', 'a', 'catch'],
-                 intensity: list[float | int] = [.8, .9, 1],
-                 stimulus: int = 5000,
-                 fixation: int | None = 100,
-                 tau: int = 100,
+                 session_in: dict[str, float] = {'v': 0.5, 'a': 0.5}, # use the right order
+                 ordered: bool = False, # True if you want to keep the order of the modalities
+                 t_in: int = 5000, # time for each stimulus in ms
+                 value_in: list[float | int] = [.8, .9, 1], # intensity values for each stimulus
+                 scaling: bool = False, # True if you want to scale the input
+                 t_fixation: int | None = 100, # time for fixation in ms
+                 # TODO: implement value_fixation usage
+                 value_fixation: int | float | None = None, # intensity value for fixation
+                 # TODO: implement max_sequential usage
+                 max_sequential: int | None = None, # maximum number of sequential trials of the same modality
+                 catch_prob: float | None = 0.5, # probability of catch trials in the session, between 0 and 1
+                 # TODO: implement inter_trial usage
+                 inter_trial: int | None = None, # inter-trial interval in ms
+                 dt: int = 20, # time step in ms
+                 tau: int = 100, # time constant in ms
                  std_inp_noise: float | int =  0.01, # standard deviation for input noise
                  baseline_inp: float | int = 0.2, # baseline input for all neurons
-                 high_output: float | int = 1.5, # desired network outputs
-                 low_output: float | int = 0.2
+                 n_out: int = 2, # number of outputs
+                 # TODO: make this a list of values
+                 high_out: float | int = 1.0, # value for high output
+                 low_out: float | int = 0.0 # value for low output
                  ):
-        """Constructor method for Task class.
-
-        Args:
-            name (str): _description_
-            modalities (list[str], optional): _description_. Defaults to ['v', 'a', 'catch'].
-                'catch' refers to giving only gaussian noise to the animal, no stimulus at all.
-            intensity (list[float | int], optional): _description_. Defaults to [.8, .9, 1].
-            stimulus (int, optional): _description_. Defaults to 5000.
-            fixation (int | None, optional): _description_. Defaults to 100.
-            tau (int, optional): _description_. Defaults to 100.
-            std_inp_noise (float | int, optional): _description_. Defaults to 0.01.
-            baseline_inp (float | int, optional): _description_. Defaults to 0.2.
-            high_output (float | int, optional): _description_. Defaults to 1.5.
-            low_output (float | int, optional): _description_. Defaults to 0.2.
-
-        Attributes:
-            name (str): _description_
-            modalities (list[str]): _description_
-            intensity (list[float | int]): _description_
-            stimulus (int): _description_
-            fixation (int | None): _description_
-            tau (int): _description_
-            std_inp_noise (float | int): _description_
-            baseline_inp (float | int): _description_
-            high_output (float | int): _description_
-            low_output (float | int): _description_
-            imin (float | int): _description_
-            imax (float | int): _description_
-            T (float | int): _description_
-            Nin (int): _description_
-            Nout (int): _description_
-            trials (dict): _description_
-        """
+        """Constructor method for Task class."""
         # Attributes from the class constructor parameters
         self.name = name
-        self.modalities = modalities
-        self.intensity = intensity
-        self.stimulus = stimulus
-        self.fixation = fixation
+        self.session_in = OrderedDict(session_in)
+        self.ordered = ordered
+        self.t_in = t_in
+        self.value_in = value_in
+        self.scaling = scaling
+        self.t_fixation = t_fixation
+        self.value_fixation = value_fixation
+        self.max_sequential = max_sequential
+        self.catch_prob = catch_prob
+        self.inter_trial = inter_trial
+        self.dt = dt
         self.tau = tau
         self.std_inp_noise = std_inp_noise
         self.baseline_inp = baseline_inp
-        self.high_output = high_output
-        self.low_output = low_output
-        # Other attributes
-        self.intensity.sort()
-        self.imin = self.intensity[0]
-        self.imax = self.intensity[-1]
-        self.T = self.fixation + self.stimulus
-        self.Nin = 3  # number of inputs
-        self.Nout = 2  # number of outputs
+        self.n_out = n_out
+        self.high_out = high_out
+        self.low_out = low_out
+
+        # Derived attributes
         self.trials = {}
         self.trials['name'] = self.name
+        self.modalities = list(OrderedDict.fromkeys(char for string in session_in for char in string))
+        self.n_in = len(self.modalities) + 1 # +1 for start cue
+        self.value_in.sort()
+        self.T = self.t_fixation + self.t_in
+        self.t = np.linspace(self.dt, self.T, int(self.T / self.dt))
+
+        # Checks
+        # TODO: check that the sum of the probabilities of session_in is 1
 
     def scale_input(self, f):
         """Method for scaling input.
@@ -83,32 +77,12 @@ class Task:
         Returns:
             _type_: _description_
         """
-        return .6*(f - self.imin) / (self.imax - self.imin)
+        return 0.6*(f - self.value_in[0]) / (self.value_in[-1] - self.value_in[0])
 
     def generate_trials(self,
                         batch_size: int = 20,
-                        scaling: bool = False,
-                        dt: int = 5,
-                        catch_prob: float = None,
                         numpy_seed: int = None):
-        """Method for generating synthetic trials.
-
-        Args:
-            batch_size (int, optional): _description_. Defaults to 20.
-            scaling (bool, optional): _description_. Defaults to False.
-            dt (int, optional): _description_. Defaults to 5.
-            catch_prob (float, optional): _description_. Defaults to None.
-            numpy_seed (int, optional): _description_. Defaults to None.
-
-        Raises:
-            ValueError: _description_
-
-        Returns:
-            _type_: _description_
-        """
-        self.dt = self.tau / dt
-        t = np.linspace(self.dt, self.T, int(self.T / self.dt))
-
+        """Method for generating synthetic trials."""
         if numpy_seed is None:
             numpy_seed = random.randrange(sys.maxsize)
         self.trials['numpy_seed'] = numpy_seed
@@ -118,94 +92,77 @@ class Task:
         # Select task condition
         # -------------------------------------------------------------------------------------
 
-        if catch_prob is not None:
-            if 'catch' in self.modalities:
-                other_mod = len(self.modalities) - 1  # number of other modalities (excluding 'catch')
+        modality_seq = []
+        for m in self.session_in.keys():
+            if self.catch_prob is not None:
+                # TODO: encode this as an actual probability
+                n_m = round(self.session_in[m] * batch_size * (1 - self.catch_prob))
+                n_catch = round(self.session_in[m] * batch_size) - n_m
+                temp_seq = n_m * [m] + n_catch * ['catch']
+                rng.shuffle(temp_seq)
+                modality_seq += list(temp_seq)
             else:
-                raise ValueError('Cannot have "catch" in modalities when using catch_prob')
-            non_catch_prob = (1 - catch_prob) / other_mod  # prob for generating trials in each of the other modalities
-            n_other_trials = int(non_catch_prob * batch_size)  # number of trials in other modalities
-            non_catch_trials = list(itertools.chain.from_iterable([[m] * n_other_trials for m in self.modalities[:-1]]))
-            modality = np.array(non_catch_trials + (batch_size - len(non_catch_trials)) * ['catch'])
-            rng.shuffle(modality)
-        else:
-            modality = rng.choice(self.modalities, batch_size)
-        # note that 'batch_size' intensity values have been sampled but they may not be used (see below)
-        intensity = rng.choice(self.intensity, (batch_size, 2))
+                n_m = int(self.session_in[m] * batch_size)
+                modality_seq += n_m * [m]
+            if not self.ordered:
+                rng.shuffle(modality_seq)
+        modality_seq = np.array(modality_seq)
 
         # -------------------------------------------------------------------------------------
         # Setup phases of trial
         # -------------------------------------------------------------------------------------
 
         phases = {}
-        phases['fixation'] = np.where(t <= self.fixation)[0]
-        phases['stimulus'] = np.where(t > self.fixation)[0]
+        phases['t_fixation'] = np.where(self.t <= self.t_fixation)[0]
+        phases['input'] = np.where(self.t > self.t_fixation)[0]
 
         # -------------------------------------------------------------------------------------
         # Trial Info
         # -------------------------------------------------------------------------------------
 
-        choice = (modality != 'catch').astype(np.int_)
+        choice = (modality_seq != 'catch').astype(np.int_)
 
-        self.trials['modality'] = modality
+        self.trials['modality_seq'] = modality_seq
         self.trials['choice'] = choice
         self.trials['phases'] = phases
-        self.trials['t'] = t
+        self.trials['t'] = self.t
 
         # -------------------------------------------------------------------------------------
         # Inputs
         # -------------------------------------------------------------------------------------
 
-        x = np.zeros((batch_size, len(t), self.Nin), dtype=np.float32)
-        for i in range(batch_size):
-            # set visual to minimum intensity in auditory and catch trials
-            if (modality[i] == 'catch') or (modality[i] == 'a'):
-                intensity[i, 0] = self.imin
+        x = np.zeros((len(self.trials['modality_seq']), len(self.t), self.n_in), dtype=np.float32)
+        sel_value_in = np.full((len(self.trials['modality_seq']), self.n_in - 1), self.value_in[0], dtype=np.float32)
+        self.modality_idx = {m: i for i, m in enumerate(self.modalities)}
 
-            # set auditory to minimum intensity in visual and catch trials
-            if (modality[i] == 'catch') or (modality[i] == 'v'):
-                intensity[i, 1] = self.imin
-
-            # intensity shouldn't be imin for non-catch trials
-            if modality[i] != 'catch':
-                if ('v' in modality[i]) and (intensity[i, 0] == self.imin):
-                    intensity[i, 0] = rng.choice(self.intensity[1:], 1)
-
-                if ('a' in modality[i]) and (intensity[i, 1] == self.imin):
-                    intensity[i, 1] = rng.choice(self.intensity[1:], 1)
-
-            if scaling:
-                # scale input
-                intensity[i, 0] = self.scale_input(intensity[i, 0])
-                intensity[i, 1] = self.scale_input(intensity[i, 1])
-
-            # input for all trials
-            x[i, phases['stimulus'], 0] = intensity[i, 0] # visual
-            x[i, phases['stimulus'], 1] = intensity[i, 1] # auditory
-            x[i, phases['stimulus'], 2] = 1 # start cue
+        for n in range(len(modality_seq)):
+            for m, idx in self.modality_idx.items():
+                if (modality_seq[n] != 'catch') and (m in modality_seq[n]):
+                    sel_value_in[n, idx] = rng.choice(self.value_in[1:], 1)
+                if self.scaling:
+                    sel_value_in[n, idx] = self.scale_input(sel_value_in[n, idx])
+                x[n, phases['input'], idx] = sel_value_in[n, idx]
+            x[n, phases['input'], len(self.modality_idx)] = 1 # start cue
 
         # store intensities in trial
-        self.trials['intensity'] = intensity
+        self.trials['sel_value_in'] = sel_value_in
 
         # add noise to inputs
-        alpha = dt/self.tau
-
+        alpha = self.dt/self.tau
         inp_noise = 1/alpha * np.sqrt(2 * alpha) * self.std_inp_noise * rng.normal(loc=0, scale=1, size=x.shape)
-
         self.trials['inputs'] = x + self.baseline_inp + inp_noise
 
         # -------------------------------------------------------------------------------------
         # target output
         # -------------------------------------------------------------------------------------
 
-        y = np.zeros((batch_size, len(t), self.Nout), dtype=np.float32)
-        for i in range(batch_size):
+        y = np.zeros((len(modality_seq), len(self.t), self.n_out), dtype=np.float32)
+        for i in range(len(modality_seq)):
+            if self.t_fixation is not None:
+                y[i, phases['t_fixation'], :] = self.low_out
 
-            if self.fixation is not None:
-                y[i, phases['fixation'], :] = self.low_output
-
-            y[i, phases['stimulus'], choice[i]] = self.high_output
-            y[i, phases['stimulus'], 1 - choice[i]] = self.low_output
+            y[i, phases['input'], choice[i]] = self.high_out
+            y[i, phases['input'], 1 - choice[i]] = self.low_out
 
         self.trials['outputs'] = y
 
@@ -228,29 +185,26 @@ class Task:
                             shared_xaxes=True,
                             vertical_spacing=0.5/n,
                             subplot_titles=[
-                                "Trial " + str(i + 1) + " - modality " + str(self.trials['modality'][i])
+                                "Trial " + str(i + 1) + " - modality " + str(self.trials['modality_seq'][i])
                                 for i in range(n)])
         showlegend = True
+        colors = [
+            '#%02x%02x%02x' % tuple(int(c * 255)
+                                    for c in colorsys.hsv_to_rgb(i / len(self.modality_idx), 1.0, 1.0))
+                                    for i in range(len(self.modality_idx))]
         for i in range(n):
-            fig.add_trace(go.Scatter(
-                name="VISUAL",
-                mode="markers+lines", x=self.trials['t'], y=self.trials['inputs'][i][:,0],
-                marker_symbol="star",
-                legendgroup="VISUAL",
-                showlegend=showlegend,
-                line_color = 'blue'
-            ), row=i+1, col=1)
-            fig.add_trace(go.Scatter(
-                name="AUDITORY",
-                mode="markers+lines", x=self.trials['t'], y=self.trials['inputs'][i][:,1],
-                marker_symbol="star",
-                legendgroup="AUDITORY",
-                showlegend=showlegend,
-                line_color = 'black'
-            ), row=i+1, col=1)
+            for m, idx in self.modality_idx.items():
+                fig.add_trace(go.Scatter(
+                    name=m,
+                    mode="markers+lines", x=self.trials['t'], y=self.trials['inputs'][i][:,idx],
+                    marker_symbol="star",
+                    legendgroup=m,
+                    showlegend=showlegend,
+                    line_color = colors[idx]
+                ), row=i+1, col=1)
             fig.add_trace(go.Scatter(
                 name="START",
-                mode="markers+lines", x=self.trials['t'], y=self.trials['inputs'][i][:,2],
+                mode="markers+lines", x=self.trials['t'], y=self.trials['inputs'][i][:,len(self.modality_idx)],
                 marker_symbol="star",
                 legendgroup="START",
                 showlegend=showlegend,
@@ -271,8 +225,8 @@ class Task:
                 line_color = 'purple'
             ), row=i+1, col=1)
             fig.add_vline(
-                x=self.fixation + self.dt, line_width=3, line_dash="dash", line_color="red")
+                x=self.t_fixation + self.dt, line_width=3, line_dash="dash", line_color="red")
             showlegend = False
-            fig.update_yaxes(range=[0, 2], row=i+1, col=1)
+            # fig.update_yaxes(range=[0, 2], row=i+1, col=1)
         fig.update_layout(height=1300, width=900, title_text="Trials")
         return fig
