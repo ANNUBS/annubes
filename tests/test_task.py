a9717363305392044e8a8a0af1fbccfc6c9c95ca
gcroci2 <crocioni.giulia@gmail.com>
2024-02-19 16:56:10 +0100

    add test for catch_prob

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,2 +1,20 @@
-def test_nothing():
-    pass
+import pytest
+
+from annubes.task import Task
+
+
+@pytest.mark.parametrize(
+    ("catch_prob", "expected"),
+    [
+        pytest.param(-1, -1, marks=pytest.mark.xfail(raises=ValueError)),
+        pytest.param(1, 1, marks=pytest.mark.xfail(raises=ValueError)),
+        pytest.param(2, 2, marks=pytest.mark.xfail(raises=ValueError)),
+        pytest.param(None, None, marks=pytest.mark.xfail(raises=ValueError)),
+        pytest.param("0.6", "0.6", marks=pytest.mark.xfail(raises=ValueError)),
+        (0.6, 0.6),
+    ],
+)
+def test_task_catch_prob(catch_prob: float | None, expected: float | None):
+    task = Task("task1", catch_prob=catch_prob)
+    assert task.name == "task1"
+    assert task.catch_prob == expected
